cbaa496f1345723b00fd53123d946ba5e9f31249
PetkoPetkov03 <komandir1612@gmail.com>
2022-11-10 11:36:11 +0200

    controls

--- Player.gd
+++ Player.gd
@@ -1,21 +1,44 @@
 extends KinematicBody2D
 
 const UP = Vector2(0, -1)
-const ACC = 20
+const GRAVITY = 40
+const MAXFALLSPEED = 450
+const MAXSPEED = 625
+const JUMPFORCE = 600
+const ACCEL = 100
 
-const GAVITY = 20
-const MAXFALLSPEED = 200
-const MAXSPEED = 80
+var facing_right = true
 
-const motion = Vector2()
+var motion = Vector2()
 func _ready():
 	pass
-
+	
 func _physics_process(delta):
-	motion.y += GAVITY
-	if Input.is_action_pressed("left"):
-		motion.x = -ACC
-	elif Input.is_action_just_pressed("right"):
-		motion.x = ACC
+	motion.y += GRAVITY
+	
+	if motion.y > MAXFALLSPEED:
+		motion.y = MAXFALLSPEED
+		
+	if facing_right == true:
+		$Sprite.scale.x = 1
 	else:
-		 motion.x = 0
+		$Sprite.scale.x = -1
+	
+	motion.x = clamp(motion.x, -MAXSPEED, MAXSPEED)
+	if Input.is_action_pressed("right"):
+		motion.x += ACCEL
+		facing_right = true
+		#$AnimationPlayer.play("run")
+	elif Input.is_action_pressed("left"):
+		motion.x -= ACCEL
+		facing_right = false
+		#$AnimationPlayer.play("run")
+	else:
+		motion.x = lerp(motion.x, 0, 0.2)
+		#$AnimationPlayer.play("Idle")
+	
+	if is_on_floor():
+		if Input.is_action_pressed("jump"):
+			motion.y = -JUMPFORCE
+			#$AnimationPlayer.play("jump")
+	motion = move_and_slide(motion,UP)
